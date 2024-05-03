f85d40af0ea905715da3643aeff4c02875afcfc1
Victor <vmoenga36@gmail.com>
2024-05-03 13:06:40 +0300

    new

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -2,10 +2,10 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Article from "../components/Article";
+import Header from "../components/Header";
 
-test("displays the text 'please pass this test'", () => {
-  render(<Article />);
+test("displays the text 'hello from the Header!'", () => {
+  render(<Header />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
